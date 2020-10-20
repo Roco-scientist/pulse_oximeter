8be5d98827566e97e95b95ac4f2d55acf0b45650
Rory Coffey <coffeyrt@gmail.com>
2020-10-20 07:38:26 -0400

    added more reads and other read types

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ fn main() {
     let mut i2c = I2c::new().unwrap();
     let address = 0x57u16;
     i2c.set_slave_address(address).unwrap();
-    let max30102_start = Max3010x::new_max30102(i2c);
-    let mut max30102 = max30102_start.into_multi_led().unwrap();
-    max30102.set_sample_averaging(SampleAveraging::Sa4).unwrap();
-    max30102.set_pulse_amplitude(Led::All, 15).unwrap();
-    max30102
+    let max30102 = Max3010x::new_max30102(i2c);
+    let mut multi_led = max30102.into_multi_led().unwrap();
+    multi_led
+        .set_sample_averaging(SampleAveraging::Sa4)
+        .unwrap();
+    multi_led.set_pulse_amplitude(Led::All, 15).unwrap();
+    multi_led
         .set_led_time_slots([
             TimeSlot::Led1,
             TimeSlot::Led2,
@@ -20,11 +22,21 @@ fn main() {
             TimeSlot::Disabled,
         ])
         .unwrap();
-    max30102.enable_fifo_rollover().unwrap();
-    let mut data = [4; 100];
-    let samples_read = max30102.read_fifo(&mut data).unwrap();
+    multi_led.enable_fifo_rollover().unwrap();
+    let mut data = [0; 10000];
+    let mut samples_read = multi_led.read_fifo(&mut data).unwrap();
 
     println!("Sample read: {:?}", samples_read);
-    // get the I2C device back
-    let i2c = max30102.destroy();
+    let mut heart_hr = max30102.into_heart_rate().unwrap();
+    heart_hr.set_sample_averaging(SampleAveraging::Sa4).unwrap();
+    heart_hr.set_pulse_amplitude(Led::All, 15).unwrap();
+    heart_hr.enable_fifo_rollover().unwrap();
+    samples_read = heart_hr.read_fifo(&mut data).unwrap();
+    println!("Herat HR Sample read: {:?}", samples_read);
+    let mut spo2 = max30102.into_oximeter().unwrap();
+    spo2.set_sample_averaging(SampleAveraging::Sa4).unwrap();
+    spo2.set_pulse_amplitude(Led::All, 15).unwrap();
+    spo2.enable_fifo_rollover().unwrap();
+    samples_read = spo2.read_fifo(&mut data).unwrap();
+    println!("SPo2 Sample read: {:?}", samples_read);
 }
