18e8361a0309b07202cb968f67dc21308903967f
Rory Coffey <coffeyrt@gmail.com>
2020-10-20 22:04:36 -0400

    unwrapped results

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rppal::i2c::I2c;
 use std::{thread::sleep, time::Duration};
 
 fn main() {
-    let mut i2c = I2c::new().unwrap();
+    let i2c = I2c::new().unwrap();
     // let address = 0x57u16;
     // i2c.set_slave_address(address).unwrap();
     let mut max30102 = Max3010x::new_max30102(i2c);
@@ -33,7 +33,7 @@ fn main() {
 
     println!("Sample read: {:?}", samples_read);
     multi_led.destroy();
-    let mut i2c = I2c::new().unwrap();
+    let i2c = I2c::new().unwrap();
     // let address = 0x57u16;
     // i2c.set_slave_address(address).unwrap();
     max30102 = Max3010x::new_max30102(i2c);
@@ -43,13 +43,13 @@ fn main() {
     heart_hr.set_sampling_rate(SamplingRate::Sps200).unwrap();
     heart_hr.set_pulse_width(LedPulseWidth::Pw411).unwrap();
     heart_hr.enable_fifo_rollover().unwrap();
-    heart_hr.shutdown();
+    heart_hr.shutdown().unwrap();
     sleep(Duration::from_secs(2));
-    heart_hr.wake_up();
+    heart_hr.wake_up().unwrap();
     samples_read = heart_hr.read_fifo(&mut data).unwrap();
     println!("Herat HR Sample read: {:?}", samples_read);
     heart_hr.destroy();
-    let mut i2c = I2c::new().unwrap();
+    let i2c = I2c::new().unwrap();
     // let address = 0x57u16;
     // i2c.set_slave_address(address).unwrap();
     max30102 = Max3010x::new_max30102(i2c);
