7482b86af381ce086c8be10dd1bbdfdeef3f92b6
Rory Coffey <coffeyrt@gmail.com>
2020-10-20 22:16:07 -0400

    added interupt status

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,8 @@ fn main() {
         .unwrap();
     multi_led.enable_fifo_rollover().unwrap();
     let mut data = [0; 500];
+    let interupt_status = multi_led.read_interrupt_status().unwrap();
+    println!("Multi interupt status: {:?}", interupt_status);
     let mut samples_read = multi_led.read_fifo(&mut data).unwrap();
 
     println!("Sample read: {:?}", samples_read);
@@ -46,6 +48,8 @@ fn main() {
     heart_hr.shutdown().unwrap();
     sleep(Duration::from_secs(2));
     heart_hr.wake_up().unwrap();
+    let interupt_status = heart_hr.read_interrupt_status().unwrap();
+    println!("HR interupt status: {:?}", interupt_status);
     samples_read = heart_hr.read_fifo(&mut data).unwrap();
     println!("Herat HR Sample read: {:?}", samples_read);
     heart_hr.destroy();
@@ -60,7 +64,10 @@ fn main() {
     spo2.set_pulse_width(LedPulseWidth::Pw411).unwrap();
     spo2.wake_up().unwrap();
     spo2.enable_fifo_rollover().unwrap();
+    let interupt_status = spo2.read_interrupt_status().unwrap();
+    println!("SPo2 interupt status: {:?}", interupt_status);
     samples_read = spo2.read_fifo(&mut data).unwrap();
     println!("SPo2 Sample read: {:?}", samples_read);
+    spo2.shutdown().unwrap();
     spo2.destroy();
 }
