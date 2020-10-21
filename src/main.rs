c54d1f393ae4a7260204ed8f4ea283adb2a8922a
Rory Coffey <coffeyrt@gmail.com>
2020-10-20 21:54:21 -0400

    added pulse width and sampling rate

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate max3010x;
 extern crate rppal;
 
-use max3010x::{Led, Max3010x, SampleAveraging, TimeSlot};
+use max3010x::{Led, LedPulseWidth, Max3010x, SampleAveraging, SamplingRate, TimeSlot};
 use rppal::i2c::I2c;
 
 fn main() {
@@ -14,6 +14,8 @@ fn main() {
         .set_sample_averaging(SampleAveraging::Sa4)
         .unwrap();
     multi_led.set_pulse_amplitude(Led::All, 15).unwrap();
+    multi_led.set_sampling_rate(SamplingRate::Sps200).unwrap();
+    multi_led.set_pulse_width(LedPulseWidth::Pw411).unwrap();
     multi_led
         .set_led_time_slots([
             TimeSlot::Led1,
@@ -29,23 +31,27 @@ fn main() {
     println!("Sample read: {:?}", samples_read);
     multi_led.destroy();
     let mut i2c = I2c::new().unwrap();
-    let address = 0x57u16;
-    i2c.set_slave_address(address).unwrap();
+    // let address = 0x57u16;
+    // i2c.set_slave_address(address).unwrap();
     max30102 = Max3010x::new_max30102(i2c);
     let mut heart_hr = max30102.into_heart_rate().unwrap();
     heart_hr.set_sample_averaging(SampleAveraging::Sa4).unwrap();
     heart_hr.set_pulse_amplitude(Led::All, 15).unwrap();
+    heart_hr.set_sampling_rate(SamplingRate::Sps200).unwrap();
+    heart_hr.set_pulse_width(LedPulseWidth::Pw411).unwrap();
     heart_hr.enable_fifo_rollover().unwrap();
     samples_read = heart_hr.read_fifo(&mut data).unwrap();
     println!("Herat HR Sample read: {:?}", samples_read);
     heart_hr.destroy();
     let mut i2c = I2c::new().unwrap();
-    let address = 0x57u16;
-    i2c.set_slave_address(address).unwrap();
+    // let address = 0x57u16;
+    // i2c.set_slave_address(address).unwrap();
     max30102 = Max3010x::new_max30102(i2c);
     let mut spo2 = max30102.into_oximeter().unwrap();
     spo2.set_sample_averaging(SampleAveraging::Sa4).unwrap();
     spo2.set_pulse_amplitude(Led::All, 15).unwrap();
+    spo2.set_sampling_rate(SamplingRate::Sps200).unwrap();
+    spo2.set_pulse_width(LedPulseWidth::Pw411).unwrap();
     spo2.enable_fifo_rollover().unwrap();
     samples_read = spo2.read_fifo(&mut data).unwrap();
     println!("SPo2 Sample read: {:?}", samples_read);
