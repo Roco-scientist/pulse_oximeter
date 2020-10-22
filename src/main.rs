89fed198a4619de7ade381cee825e902d31d2151
Rory Coffey <coffeyrt@gmail.com>
2020-10-21 21:37:46 -0400

    printed data and set address

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ use rppal::i2c::I2c;
 use std::{thread::sleep, time::Duration};
 
 fn main() {
-    let i2c = I2c::new().unwrap();
-    // let address = 0x57u16;
-    // i2c.set_slave_address(address).unwrap();
+    let mut i2c = I2c::new().unwrap();
+    let address = 0x57u16;
+    i2c.set_slave_address(address).unwrap();
     let mut max30102 = Max3010x::new_max30102(i2c);
     let mut multi_led = max30102.into_multi_led().unwrap();
     // enable interupts
     multi_led.enable_fifo_almost_full_interrupt().unwrap();
-    multi_led.enable_new_fifo_data_ready_interrupt().unwrap();
+    // multi_led.enable_new_fifo_data_ready_interrupt().unwrap();
     // sample average 4, fifo rollover false, fifo almost full 17
     multi_led
         .set_sample_averaging(SampleAveraging::Sa4)
@@ -42,7 +42,7 @@ fn main() {
     println!("Multi interupt status: {:?}", interupt_status);
     let mut samples_read = multi_led.read_fifo(&mut data).unwrap();
     println!("Sample read: {:?}", samples_read);
-    println!("Multi data: {:?}", data);
+    println!("Multi data: {:?}", &data[0..10]);
     // next
     let mut heart_hr = multi_led.into_heart_rate().unwrap();
     heart_hr.set_sample_averaging(SampleAveraging::Sa4).unwrap();
@@ -57,7 +57,7 @@ fn main() {
     println!("HR interupt status: {:?}", interupt_status);
     samples_read = heart_hr.read_fifo(&mut data).unwrap();
     println!("HR Sample read: {:?}", samples_read);
-    println!("HR data: {:?}", data);
+    println!("HR data: {:?}", &data[0..10]);
     // next
     let mut spo2 = heart_hr.into_oximeter().unwrap();
     spo2.set_sample_averaging(SampleAveraging::Sa4).unwrap();
@@ -70,6 +70,7 @@ fn main() {
     println!("SPo2 interupt status: {:?}", interupt_status);
     samples_read = spo2.read_fifo(&mut data).unwrap();
     println!("SPo2 Sample read: {:?}", samples_read);
+    println!("SPo2 data: {:?}", &data[0..10]);
     spo2.shutdown().unwrap();
     spo2.destroy();
 }
